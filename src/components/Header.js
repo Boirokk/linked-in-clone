955887af3f6fa4fc18fb2597eff79a5c7c065a2e
Chad C <chadzx10@gmail.com>
2021-05-16 10:29:43 -0400

    NavList

--- src/components/Header.js
+++ src/components/Header.js
@@ -99,13 +99,30 @@ const Nav = styled.nav`
 `;
 
 const NavListWrap = styled.ul`
-  
+  display: flex;
+  flex-wrap: nowrap;
+  list-style: none;
 `;
 
 const NavList = styled.li`
-  
-`;
+  display: flex;
+  align-items: center;
+  a {
+      align-items: center;
+      background: transparent;
+      display: flex;
+      flex-direction: column;
+      font-size: 12px;
+      font-weight: 400;
+      justify-content: center;
+      line-height: 1.5;
+      min-height: 42px;
+      min-width: 80px;
+      position: relative;
+      text-decoration: none;
 
+  }
+`;
 
 
 export default Header;
